e6874bddaee869f2491c5229b818f8cd3fd4bf10
Danamir <danamir@danamir.com>
2018-03-13 10:45:10 +0100

    GitLab swimlanes grouping
    Added swimlanes grouping by label category / user / all label on click / shift + click / ctrl + click.

--- gitlab-swimlanes.user.js
+++ gitlab-swimlanes.user.js
@@ -2,11 +2,10 @@
 // @name            Gitlab swimlanes
 // @namespace       https://github.com/Danamir/gitlab-swimlanes/
 // @version         0.2
-// @description     Add swimlanes to Gilab issues board
+// @description     Add simlanes to Gilab issues board
 // @author          Danamir
 // @match           http*://*/*/boards
 // @match           http*://*/*/boards?*
-// @match           GITLAB_URL/boards*
 // @require         https://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
 
@@ -17,19 +16,26 @@
  *   - Append "--" or "-Category name-" to the label descriptions you want to use as swimlanes.
  *   - Alter the script to replace GITLAB_URL by your Gitlab server url.
  *   - Load the script with Tampermonkey: http://tampermonkey.net/ .
- *   - Click the Swimlanes button beside the search fields to toggle the swimlanes display.
+ *   - Click the Swimlanes button beside the search fields to toggle the swimlanes display :
+ *      - Single click group by category labels.
+ *      - Shift click group by assigned users.
+ *      - Ctrl click group by all non-category labels. (Warning: Can be slow).
  *
  * Notes:
  *   - All interactions are disabled in the swimlanes, but still available in the main board.
  *   - The swimlanes_* global variables can be altered to customise the display.
  */
+// configuration
 var swimlane_min_height = 200;
 var swimlane_max_height = 640; // -1 for unlimited height
 var swimlane_tag = 'h4';
 var swimlane_font_size = '16px';
 var swimlane_animate = true;
+var swimlane_types = ["label", "user", "all"];  // behaviour of click, shift + click, ctrl + click
 
+// local variables
 var hidden_lanes = [];
+var swimlane_type = "";
 
 /**
  * Get URL parameters.
@@ -153,7 +159,7 @@ function has_swimlane(swimlanes, title) {
 function display_swimlanes() {
     var main_board;
     main_board = $('#main-list');
-    if (main_board.length == 0) {
+    if (main_board.length === 0) {
         main_board = $('.boards-list');
         main_board.prop('id', 'main-list');
     }
@@ -161,48 +167,123 @@ function display_swimlanes() {
     main_board.before('<'+swimlane_tag+' class="board-inner" id="main-list_title" style="font-size: '+swimlane_font_size+'; font-weight: bold; padding: 6px 20px; margin-left: 8px; margin-right: 8px; margin-bottom: -14px;">Main board</'+swimlane_tag+'>');
 
     var swimlanes = {};
-    var reg = /^(.*)\s*-(.*)-$/;
+
+    var list_titles = [];
+    $('.board-title-text', main_board).each(function () {
+        var text = $(this).text().trim();
+        if (text && $.inArray(text, list_titles) === -1) {
+            list_titles.push(text);
+        }
+    });
 
     do {
         var current_board = main_board.clone();
         var current_swimlane_title = undefined;
-        var swimlane_button = undefined;
+        var swimlane_item = undefined;
 
         $('.card', current_board).each(function () {
             var card = $(this);
             var keep_card = false;
 
-            $('.card-footer button', card).each(function () {
-                var button = $(this);
-                var title = button.prop("title");
-                if (!title) {
-                    title = button.attr("data-original-title");
-                }
-
-                if (title) {
-                    var match = reg.exec(title);
-
-                    if (!match) {
-                        // not a swimline title
-                        return true;
-                    } else if (current_swimlane_title && title != current_swimlane_title) {
-                        // not current swimlane
-                        return true;
-                    } else if (has_swimlane(swimlanes, title)) {
-                        // swimlane already exists
-                        return true;
+            if (swimlane_type === "label") {
+                $('.card-footer button', card).each(function () {
+                    var item = $(this);
+                    var title = "";
+                    if(item.prop("title")) {
+                        title = item.prop("title");
+                    } else if (item.attr("data-original-title")) {
+                        title = item.attr("data-original-title");
                     }
 
-                    current_swimlane_title = title;
-                    swimlane_button = button;
-                    keep_card = true;
-                }
+                    if (title) {
+                        var match = /^(.*)\s*-(.*)-$/.exec(title);
 
-            });
+                        if (!match) {
+                            // not a category label
+                            return true; // continue
+                        } else if (current_swimlane_title && title !== current_swimlane_title) {
+                            // not current swimlane
+                            return true; // continue
+                        } else if (has_swimlane(swimlanes, title)) {
+                            // swimlane already exists
+                            return true; // continue
+                        }
+
+                        current_swimlane_title = title;
+                        swimlane_item = item;
+                        keep_card = true;
+                    }
+                });
+            } else if (swimlane_type === "all") {
+                $('.card-footer button', card).each(function () {
+                    var item = $(this);
+                    var title = item.text().trim();
+
+                    var tooltip = "";
+                    if(item.prop("title")) {
+                        tooltip = item.prop("title");
+                    } else if (item.attr("data-original-title")) {
+                        tooltip = item.attr("data-original-title");
+                    }
+
+                    if (title) {
+                        var match = /^(.*)\s*-(.*)-$/.exec(tooltip);
+
+                        if (match) {
+                            // category label
+                            return true; // continue
+                        } else if ($.inArray(title, list_titles) > -1) {
+                            // already displayed as list
+                            return true; // continue
+                        } else if (current_swimlane_title && title !== current_swimlane_title) {
+                            // not current swimlane
+                            return true; // continue
+                        } else if (has_swimlane(swimlanes, title)) {
+                            // swimlane already exists
+                            return true; // continue
+                        }
+
+                        current_swimlane_title = title;
+                        swimlane_item = item;
+                        keep_card = true;
+                    }
+                });
+            } else if (swimlane_type === "user") {
+                $('.card-assignee img', card).each(function () {
+                    var item = $(this);
+                    var title = "";
+                    if(item.prop("title")) {
+                        title = item.prop("title");
+                    } else if (item.attr("data-original-title")) {
+                        title = item.attr("data-original-title");
+                    }
+
+                    if (title) {
+                        var match = /^Assi\S* \S+ (.*)$/.exec(title);
+
+                        if (!match) {
+                            // not a swimline title
+                            return true; // continue
+                        } else if (current_swimlane_title && title !== current_swimlane_title) {
+                            // not current swimlane
+                            return true; // continue
+                        } else if (has_swimlane(swimlanes, title)) {
+                            // swimlane already exists
+                            return true; // continue
+                        }
+
+                        current_swimlane_title = title;
+                        swimlane_item = item;
+                        keep_card = true;
+                    }
+                });
+            } else {
+                console.log("Unknown swimlane type: "+swimlane_type);
+            }
 
             if (!keep_card) {
                 card.remove();
-                return true;
+                return true; // continue
             }
         });
 
@@ -210,10 +291,14 @@ function display_swimlanes() {
         if (current_swimlane_title) {
             current_board.prop("id", swimlane_id(current_swimlane_title));
             current_board.html('<div class="sw-inner-board">'+current_board.html()+'</div>');
+
+            // update tags
             $('.board-list-count', current_board).remove();
             $('.issue-count-badge', current_board).remove();
             $('.issue-count-badge-count', current_board).remove();
             $('.board-delete', current_board).remove();
+            $('.is-expandable', current_board).removeClass('is-expandable');
+            $('.user-can-drag', current_board).removeClass('user-can-drag');
 
             // css
             $('ul', current_board).css({'min-height': swimlane_min_height+'px'});
@@ -225,16 +310,26 @@ function display_swimlanes() {
 
 
             // title
-            var title = reg.exec(current_swimlane_title);
-            if (swimlane_button) {
-                swimlane_button.css({border: "0", outline: "0", position: "absolute", right: "10px"});
-                swimlane_button.removeClass("has-tooltip");
-                swimlane_button.prop("title", "");
-                swimlane_button = swimlane_button.prop('outerHTML');
-            } else {
-                swimlane_button = "";
+            var title = "";
+            var item = "";
+            if (swimlane_type === "label") {
+                var title = /^(.*)\s*-(.*)-$/.exec(current_swimlane_title);
+                title = title[1]+'<span style="font-size: 0.65em; vertical-align: top; font-style: italic;">'+title[2]+'</span>';
+            } else if (swimlane_type === "all") {
+                title = current_swimlane_title;
+            } else if (swimlane_type === "user") {
+                var title = /^Assi\S* \S+ (.*)$/.exec(current_swimlane_title);
+                title = title[1];
             }
-            current_board.first().prepend('<'+swimlane_tag+' class="board-inner" id="'+swimlane_id(current_swimlane_title)+'_title" style="cursor: pointer; font-size: '+swimlane_font_size+'; padding: 6px;"><i aria-hidden="true" class="fa fa-fw fa-caret-down"></i>'+title[1]+'<span style="font-size: 0.65em; vertical-align: top; font-style: italic;">'+title[2]+'</span>'+swimlane_button+'</'+swimlane_tag+'>');
+
+            if (swimlane_item.length > 0) {
+                swimlane_item.css({border: "0", outline: "0", position: "absolute", right: "10px"});
+                swimlane_item.removeClass("has-tooltip");
+                swimlane_item.prop("title", "");
+                item = swimlane_item.prop('outerHTML');
+            }
+
+            current_board.first().prepend('<'+swimlane_tag+' class="board-inner" id="'+swimlane_id(current_swimlane_title)+'_title" style="cursor: pointer; font-size: '+swimlane_font_size+'; padding: 6px;"><i aria-hidden="true" class="fa fa-fw fa-caret-down"></i>'+title+item+'</'+swimlane_tag+'>');
 
             swimlanes[current_swimlane_title] = current_board;
         }
@@ -251,7 +346,7 @@ function remove_swimlanes() {
     $('.boards-list').each(function () {
         var board = $(this);
 
-        if (board.prop("id") && board.prop("id") != 'main-list') {
+        if (board.prop("id") && board.prop("id") !== 'main-list') {
             if (swimlane_animate) {
                 $('ul', board).hide(800);
                 $('.sw-inner-board', board).hide(200);
@@ -279,8 +374,16 @@ $(document).ready(function() {
 
         $('.board-extra-actions').append(btn);
 
-        $('.btn-display-swimlanes').on("click", function () {
-            if(!btn.attr("aria-pressed") || btn.attr("aria-pressed") == "false") {
+        $('.btn-display-swimlanes').on("click", function (e) {
+            if (e && e.shiftKey && swimlane_types.length > 1) {
+                swimlane_type = swimlane_types[1];
+            } else if (e && e.ctrlKey && swimlane_types.length > 2) {
+                swimlane_type = swimlane_types[2];
+            } else {
+                swimlane_type = swimlane_types[0];
+            }
+
+            if(!btn.attr("aria-pressed") || btn.attr("aria-pressed") === "false") {
                 display_swimlanes();
             } else {
                 remove_swimlanes();

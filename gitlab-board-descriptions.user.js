49ac0ad4afec8215e462874d63600e53b3fa861c
Danamir <danamir@danamir.com>
2018-05-22 10:24:57 +0200

    GitLab board issues description
    Updated to version 0.3 .

--- gitlab-board-descriptions.user.js
+++ gitlab-board-descriptions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            GitLab board descriptions
 // @namespace       https://github.com/Danamir/gitlab-userscripts/
-// @version         0.2
+// @version         0.3
 // @description     Display issues description in GiLab issues board
 // @author          Danamir
 // @match           http*://*/*/boards
